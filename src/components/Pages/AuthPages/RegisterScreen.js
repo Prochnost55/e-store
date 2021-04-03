1eabe85406bfc622c3fc3d552393e52253dc2e55
Abhishek Singh <aks3555@gmail.com>
2021-04-03 16:28:48 +0530

    changes for backend

--- src/components/Pages/AuthPages/RegisterScreen.js
+++ src/components/Pages/AuthPages/RegisterScreen.js
@@ -66,31 +66,30 @@ const RegisterScreen = ({history}) => {
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-      },
-    };
-
-    try {
-      const { data } = await axios.post(
-        "http://localhost:3000/api/user/register",
-        { name, email, password },
-        config
-      );
-      
-      setErrorMessage("");
-      setSuccessMessage("Successfully Registered !");
-      localStorage.setItem("user", JSON.stringify(data));
-      history.push("/login");
-
-    } catch (error) {
-      setErrorMessage("User Already Exists!");
-      setSuccessMessage("");
-    }
+  const handleSubmit = event => {
+    event.preventDefault();
+    
+    axios.post( "http://localhost:3030/api/user/register",
+        {
+          params:{
+            name: name,
+          }
+        },{auth:{
+          username: email,
+          password: password
+        }}
+      ).then((response)=> {
+        if(response && response.status == 200){
+          setErrorMessage("");
+          setSuccessMessage("Successfully Registered !");
+          // localStorage.setItem("user", JSON.stringify(data));
+          // history.push("/login");
+        }
+      })
+      .catch((error) => {
+        setErrorMessage("User Already Exists!");
+        setSuccessMessage("");
+      })
   };
 
   return (
